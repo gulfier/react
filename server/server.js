1beb83e208a5bf7c1b448dd739ac8d35421bb3f2
GULFIER LEONEL LOPEZ VAZQUEZ <gllopezv@ad.elektra.com.mx>
2020-12-23 15:50:01 -0600

    d0999

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ const router = require('./routes/routes.js');
 
 const app = express();
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../client'));
-app.use(express.static(path.join(__dirname, '../client')));
+app.set('views', path.join(__dirname, '../src'));
+app.use(express.static(path.join(__dirname, '../src')));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
 app.use('/', router);
